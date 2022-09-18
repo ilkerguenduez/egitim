f487ea8c4cab613baa530e88f76f4c98753cd46c
İlker Gündüz <ilkergndz3500@gmail.com>
2022-09-19 01:03:41 +0300

    db

--- database/migrations/2022_09_18_212121_create_yazarlars_table.php
+++ database/migrations/2022_09_18_212121_create_yazarlars_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('yazarlars', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('permission')->default(0);
-            $table->rememberToken();
+            $table->string('selflink');
+            $table->string('image');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('yazarlars');
     }
 };
