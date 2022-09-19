944d16fb0680e01f51d6cc802bbd8680d3a41628
İlker Gündüz <ilkergndz3500@gmail.com>
2022-09-20 01:53:06 +0300

    db

--- database/migrations/2022_09_19_224918_create_kitaplars_table.php
+++ database/migrations/2022_09_19_224918_create_kitaplars_table.php
@@ -13,13 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('yazarlars', function (Blueprint $table) {
+        Schema::create('kitaplars', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('selflink');
-            $table->string('image');
-            $table->text('bio')->nullable();
-            $table->timestamps();
+            $table->sti            $table->timestamps();
         });
     }
 
@@ -30,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('yazarlars');
+        Schema::dropIfExists('kitaplars');
     }
 };
